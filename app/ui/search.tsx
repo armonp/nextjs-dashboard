80319624ea1db43071326e76d25959160748087e
armonp <armon.porter@gmail.com>
2024-03-11 23:18:40 -0400

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,23 +2,24 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useDebouncedCallback } from 'use-debounce';
-import { useSearchParams, usePathname,  useRouter } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const { replace } = useRouter()
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`)
-const params = new URLSearchParams(searchParams)
-if (term) {
-  params.set('query', term)
-} else {
-  params.delete('query')
-}
-replace(`${pathname}?${params.toString()}`)
-  }, 300)
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -29,7 +30,7 @@ replace(`${pathname}?${params.toString()}`)
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value)
+          handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
